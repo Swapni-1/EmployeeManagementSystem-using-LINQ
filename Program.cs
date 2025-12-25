42216d92417ce145473a3c60b2e4edfdf0054d05
Swapni-1 <swapnillakra822@gmail.com>
2025-12-25 23:39:25 +0530

    Done upto question 6

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿namespace EMSLINQ
+﻿using EMSLINQ.Models;
+
+namespace EMSLINQ
 {
     class Program
     {
@@ -6,7 +8,96 @@
         {
            using var context = new EmployeeManagementContext();
            
-           
+         /*
+          1. Query Syntax & Method Syntax
+          
+            Question:
+            "Write a LINQ query to retrieve the names of all employees in the 'HR' department
+            using both Query Syntax and Method Syntax."
+          */
+
+         var QSQuestion1 = from employee in context.Employees
+                where employee.Department.DepartmentName == "HR"
+                    select $"{employee.FirstName} {employee.LastName}";
+
+         var MSQuestion1 = context.Employees.Where(employee =>
+                 employee.Department.DepartmentName == "HR")
+             .Select(employee => $"{employee.FirstName} {employee.LastName}");
+         
+         /*
+          2. Lambda Expressions and Anonymous Types
+            • Question:
+            "Write a LINQ query to get a list of employees with their names and roles. Use lambda
+            expressions and return the result as an anonymous type."
+          */
+
+         var LEATQuestion2 = context.Employees.Select(employee => new
+         {
+             Name = $"{employee.FirstName} {employee.LastName}",
+             Role = employee.Role.RoleName
+         });
+         
+         /*
+          3. Deferred & Immediate Execution
+            • Question:
+            "Write a LINQ query that uses deferred execution to retrieve all employees in the
+            'Marketing' department and then force immediate execution by converting the result to a
+            list."
+          */
+
+         var DfQuestion3 = context.Employees.Where(employee => employee.Department.DepartmentName == "Marketing");
+
+         var IEQuestion3 = context.Employees.ToList();
+
+         
+         /*
+          4. Using Where, OrderBy, and ThenBy
+            • Question:
+            "Write a LINQ query that returns employees in the 'Engineering' department, sorted first by
+            LastName in ascending order and then by FirstName in descending order. Use Where and
+            OrderBy, followed by ThenBy."
+          */
+
+         var WOTQuestion4 = context.Employees
+             .Where(employee => employee.Department.DepartmentName == "Engineering")
+             .OrderBy(employee => employee.LastName)
+             .ThenByDescending(employee => employee.FirstName);
+
+         /*
+          5. Select and SelectMany
+            • Question:
+            "Write a LINQ query to retrieve the department names and a list of all employees working in
+            each department. Use Select and SelectMany to flatten the employee list."
+          */
+
+         var departmentEmpoyees = context.Departments
+             .Select(department => new
+             {
+                 Department = department.DepartmentName,
+                 Employee = department.Employees.Select(employee => $"{employee.FirstName} {employee.LastName}")
+             });
+
+         var allEmployeesFlattened = context.Departments
+             .SelectMany(department => department.Employees);
+         
+         /*
+          6. FirstOrDefault, LastOrDefault
+            • Question:
+            "Write a LINQ query to find the first employee hired in 2020 and the last employee who left
+            the company in 2019 using FirstOrDefault and LastOrDefault."
+          */
+
+         var firstHired2020 = context.Employees
+             .Where(employee => employee.HireDate.Year == 2020)
+             .OrderBy(employee => employee.HireDate)
+             .FirstOrDefault();
+
+         var firstHired2019 = context.Employees
+             .Where(employee => employee.HireDate.Year == 2019)
+             .OrderBy(employee => employee.HireDate)
+             .LastOrDefault();
+
+         
         }
     }
 }
--- obj/Debug/net10.0/EMSLINQ.AssemblyInfo.cs
+++ obj/Debug/net10.0/EMSLINQ.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EMSLINQ")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3fd94a9aa43db69712a04ff604fe88f62eeaecdf")]
 [assembly: System.Reflection.AssemblyProductAttribute("EMSLINQ")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EMSLINQ")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
